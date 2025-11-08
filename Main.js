e0273f9acf49e7e761818caaffe77767cdd526df
Straitis <Straitis@protonmail.com>
2025-11-07 19:05:21 -0800

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,11 @@
+
+
+document.body.style.border = "5px solid red";
+console.log("STRATCOM")
+
+ document.addEventListener("click", (e) => {
+    /**
+     * Given the name of a beast, get the URL to the corresponding image.
+     */
+    console.log("STRATCOM")
+})
